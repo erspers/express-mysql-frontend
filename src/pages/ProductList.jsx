b6764c11d2c8c5fede966ce8fbefa011acc11353
damm <119106669+S4f3z@users.noreply.github.com>
2025-11-09 22:28:52 +0800

    Update ProductList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -81,7 +81,7 @@ const ProductList = () => {
 
   return (
     <div className="product-page">
-      <h1>Simple Products CRUD Application</h1>
+      <h1>GMJFT Haircare Products</h1>
 
       <section className="form-section">
         <ProductForm
